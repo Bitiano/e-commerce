f93fe6ff6f3d1641fdb1ef43f89435ac1918eb72
AUGUSTO HENRIQUE RAMOS BITIANO <augusto.hrbitiano@senacsp.edu.br>
2024-08-27 20:14:23 -0300

    fix: init backend

---
+++ backend/src/main/java/senac/pi/ecommerce/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package senac.pi.ecommerce.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/senac/pi/ecommerce/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package senac.pi.ecommerce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
