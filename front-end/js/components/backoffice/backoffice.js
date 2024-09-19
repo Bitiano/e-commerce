94e53902469acbb117753a62dc0d3f6978ce81f2
vitor Cobeio <vitorcobeio7@gmail.com>
2024-09-19 17:42:25 -0300

    Update

--- front-end/js/components/backoffice/backoffice.js
+++ front-end/js/components/backoffice/backoffice.js
@@ -1,39 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const boxBtns = document.getElementById('box-btns');
-    const token = localStorage.getItem('token');
-
-    async function fetchUserData(token) {
-        try {
-            const response = await fetch(`http://localhost:8080/usuario/informacoes?jwtToken=${token}`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            if (response.ok) {
-                const user = await response.json();
-                return user;
-            } else {
-                throw new Error('Erro ao buscar dados do usuário');
-            }
-        } catch (error) {
-            alert('Erro ao buscar dados do usuário');
-        }
-    }
+    const boxBtns = document.querySelector('#box-btns');
+    const userType = localStorage.getItem('userType');
 
     async function initBackoffice() {
-        const user = await fetchUserData(token);
-        if (user) {
-            if (user.grupo === 'ADMIN') {
-                boxBtns.innerHTML = `
-                    <a href="users-list.html" class="btn btn-users-list">Lista de Usuários</a>
-                    <a href="products-list.html" class="btn btn-products-list">Lista de Produtos</a>
-                `;
-            } else if (user.grupo === 'ESTOQUISTA') {
-                boxBtns.innerHTML = `
-                    <a href="products-list.html" class="btn btn-products-list">Lista de Produtos</a>
-                `;
-            }
+        if (userType === 'ADMIN') {
+            boxBtns.innerHTML = `
+                <a href="users-list.html" class="btn btn-users-list">Lista de Usuários</a>
+                <a href="products-list.html" class="btn btn-products-list">Lista de Produtos</a>
+            `;
+        } else if (userType === 'ESTOQUISTA') {
+            boxBtns.innerHTML = `
+                <a href="products-list.html" class="btn btn-products-list">Lista de Produtos</a>
+            `;
         }
     }
 
--- front-end/js/components/form/login/loginFormHandler.js
+++ front-end/js/components/form/login/loginFormHandler.js
@@ -1,10 +1,10 @@
-document.getElementById('login-form').addEventListener('submit', async function(event) {
+document.getElementById('login-form').addEventListener('submit', async function (event) {
     event.preventDefault();
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch('http://localhost:8080/usuario/login', { 
+    const response = await fetch('http://localhost:8080/usuario/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -17,9 +17,26 @@ document.getElementById('login-form').addEventListener('submit', async function(
     if (response.ok) {
         const token = await response.text();
         localStorage.setItem('token', token);
+
+        const userInfoResponse = await fetch(`http://localhost:8080/usuario/informacoes?jwtToken=${token}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if (userInfoResponse.ok) {
+            const user = await userInfoResponse.json();
+            localStorage.setItem('userType', user.grupo);
+        } else {
+            messageElement.textContent = 'Erro ao buscar informações do usuário';
+            messageElement.style.color = 'red';
+            return;
+        }
+
         messageElement.textContent = 'Login bem-sucedido!';
         messageElement.style.color = 'green';
-        window.location.href = './pages/backoffice.html'; 
+        window.location.href = './pages/backoffice.html';
     } else {
         messageElement.textContent = `Usuario ou senha inválidos!`;
         messageElement.style.color = 'red';
