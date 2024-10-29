030d35af49c764e534c6667a9b5d046264d82ad3
Guilherme Henrique S. Pereira <guilhermehsp26@gmail.com>
2024-10-29 17:24:56 -0300

    feat: Adiciona inicialização de dados e configuração de CORS
    Adiciona a inicialização de dados no banco de dados, incluindo um usuário e um cliente. Também adiciona a configuração do CORS para permitir requisições de qualquer origem, método e cabeçalho.

--- backend/src/main/java/senac/pi/ecommerce/backend/config/DataInitializer.java
+++ backend/src/main/java/senac/pi/ecommerce/backend/config/DataInitializer.java
@@ -6,12 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import senac.pi.ecommerce.backend.models.Usuario;
 import senac.pi.ecommerce.backend.repository.UsuarioRepository;
+import senac.pi.ecommerce.backend.models.Cliente;
+import senac.pi.ecommerce.backend.repository.ClienteRepository;
+
+import java.time.LocalDate;
+import java.util.Collections;
 
 @Configuration
 public class DataInitializer {
 
     @Bean
-    public CommandLineRunner initData(UsuarioRepository usuarioRepository) {
+    public CommandLineRunner initData(UsuarioRepository usuarioRepository, ClienteRepository clienteRepository) {
         return args -> {
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
             String encodedPassword = encoder.encode("123");
@@ -25,6 +30,19 @@ public class DataInitializer {
             usuario.setCpf("12345678900");
 
             usuarioRepository.save(usuario);
+
+            Cliente cliente = new Cliente();
+            cliente.setEmail("cliente@example.com");
+            cliente.setCpf("98765432100");  
+            cliente.setNomeCompleto("Maria da Silva");
+            cliente.setDataNascimento(LocalDate.of(1990, 1, 1));
+            cliente.setGenero("Feminino");
+            cliente.setEnderecos(Collections.emptyList());
+            cliente.setSenha(encodedPassword);
+            cliente.setPedidos(Collections.emptyList());
+
+            clienteRepository.save(cliente);
         };
     }
+
 }
--- backend/src/main/java/senac/pi/ecommerce/backend/config/WebConfig.java
+++ backend/src/main/java/senac/pi/ecommerce/backend/config/WebConfig.java
@@ -10,9 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:5501")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
